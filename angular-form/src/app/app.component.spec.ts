7c63bbffee7260c490e48de725feacf33406e26d
VyDee <vy.duong@cuw.edu>
2020-06-13 00:19:43 -0500

    update

--- angular-form/src/app/app.component.spec.ts
+++ angular-form/src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ps-demo'`, () => {
+  it(`should have as title 'angular-form'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('ps-demo');
+    expect(app.title).toEqual('angular-form');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to ps-demo!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to angular-form!');
   });
 });

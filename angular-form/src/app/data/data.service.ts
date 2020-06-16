d69a409f9f25bfb82d156fea1f660ba68dc56ac8
VyDee <vy.duong@cuw.edu>
2020-06-15 23:43:44 -0500

    Http set and post data to the endpoint

--- angular-form/src/app/data/data.service.ts
+++ angular-form/src/app/data/data.service.ts
@@ -1,16 +1,18 @@
 import { Injectable } from '@angular/core';
 import { UserSettings } from './user-setting';
 import { Observable, of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
-  postUserSettingsForm(userSettings: UserSettings) : Observable<UserSettings> {
-    return of(userSettings);
+  postUserSettingsForm(userSettings: UserSettings) : Observable<any> {
+    return this.http.post('https://putsreq.com/b6cE92tW4aTgXIgCWawi', userSettings);
+    // return of(userSettings);
   }
 
 }
